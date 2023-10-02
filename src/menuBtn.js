54af5363bb43b8890c9d02ac237e08195a516970
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2023-10-01 22:28:35 -0400

    added a learn more section as well made text move to acoordian so it doesnt oveplap.

--- src/menuBtn.js
+++ src/menuBtn.js
@@ -13,3 +13,31 @@ menuBtn.onclick = function(){
         menu.src = "img/menuIcon.png"
     }
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const accordionItems = document.querySelectorAll('input[type="radio"][name="accordian"]');
+    const learnMoreSection = document.getElementById('learnmore-section');
+    const moveableButtons = document.querySelectorAll('.learnmore-buttons');
+
+    accordionItems.forEach(item => {
+        item.addEventListener("change", function () {
+            if (item.checked) {
+                // An accordion item is checked (open), so calculate and set the position
+                const openAccordionItem = document.querySelector(`#${item.id} + .content`);
+                const openAccordionItemHeight = openAccordionItem.scrollHeight;
+                learnMoreSection.style.transform = `translateY(${openAccordionItemHeight}px)`;
+
+                // Move the buttons with the same offset
+                moveableButtons.forEach(button => {
+                    button.style.transform = `translateY(${openAccordionItemHeight}px)`;
+                });
+            } else {
+                // No accordion items are checked (closed), so reset the positions
+                learnMoreSection.style.transform = 'translateY(0)';
+                moveableButtons.forEach(button => {
+                    button.style.transform = 'translateY(0)';
+                });
+            }
+        });
+    });
+});
