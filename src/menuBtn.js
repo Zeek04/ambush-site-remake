3fcc5831d4207e0c1f9c99834e0bbc847776c0ad
Ezequiel Cruz Collazo <Cruzezequiel81@gmail.com>
2023-10-02 01:57:07 -0400

    Fixed map placement as well finished footer

--- src/menuBtn.js
+++ src/menuBtn.js
@@ -1,6 +1,10 @@
 let menuBtn = document.getElementById("menuBtn")
 let sideNav = document.getElementById('sideNav')
 let menu = document.getElementById("menu")
+const accordionItems = document.querySelectorAll('input[type="radio"][name="accordian"]');
+const learnMoreSection = document.getElementById('learnmore-section');
+const moveableButtons = document.querySelectorAll('.learnmore-buttons');
+
 
 sideNav.style.right = "-250px"
 
@@ -14,11 +18,8 @@ menuBtn.onclick = function(){
     }
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const accordionItems = document.querySelectorAll('input[type="radio"][name="accordian"]');
-    const learnMoreSection = document.getElementById('learnmore-section');
-    const moveableButtons = document.querySelectorAll('.learnmore-buttons');
 
+document.addEventListener("DOMContentLoaded", function () {
     accordionItems.forEach(item => {
         item.addEventListener("change", function () {
             if (item.checked) {
@@ -43,4 +44,30 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 document.getElementById('map-overlay').addEventListener('click', function() {
     document.querySelector('.map-container').style.filter = 'blur(0px)'; // Remove the blur
+});
+
+accordionItems.forEach(item => {
+    item.addEventListener("change", function () {
+        if (item.checked) {
+            // An accordion item is checked (open), so calculate and set the position
+            const openAccordionItem = document.querySelector(`#${item.id} + .content`);
+            const openAccordionItemHeight = openAccordionItem.scrollHeight;
+
+            // Update the position of map-section and footer
+            document.querySelector('.map-section').style.transform = `translateY(${openAccordionItemHeight}px)`;
+            document.querySelector('.footer').style.transform = `translateY(${openAccordionItemHeight}px)`;
+
+            // Move the buttons with the same offset
+            moveableButtons.forEach(button => {
+                button.style.transform = `translateY(${openAccordionItemHeight}px)`;
+            });
+        } else {
+            // No accordion items are checked (closed), so reset the positions
+            document.querySelector('.map-section').style.transform = 'translateY(0)';
+            document.querySelector('.footer').style.transform = 'translateY(0)';
+            moveableButtons.forEach(button => {
+                button.style.transform = 'translateY(0)';
+            });
+        }
+    });
 });
